1e89e8bdae64bce6e31417538841ba435ec5e6ce
ddangchani <dang11230@gmail.com>
2023-11-28 22:24:03 +0900

    commit

--- runscript.ps1
+++ runscript.ps1
@@ -1,15 +1,9 @@
 # Script to run experiments
-python train.py --dependence_type=1 --node_size=10 --seed=1 --flow_type='DAGGNN' --dependence_prop=0.5
-python train.py --dependence_type=1 --node_size=10 --seed=2 --flow_type='DAGGNN' --dependence_prop=0.5
-python train.py --dependence_type=1 --node_size=10 --seed=3 --flow_type='DAGGNN' --dependence_prop=0.5
-python train.py --dependence_type=1 --node_size=10 --seed=4 --flow_type='DAGGNN' --dependence_prop=0.5
-python train.py --dependence_type=1 --node_size=10 --seed=5 --flow_type='DAGGNN' --dependence_prop=0.5
-
-python train.py --dependence_type=1 --node_size=10 --seed=1 --flow_type='IAF' --dependence_prop=0.5
-python train.py --dependence_type=1 --node_size=10 --seed=2 --flow_type='IAF' --dependence_prop=0.5
 python train.py --dependence_type=1 --node_size=10 --seed=3 --flow_type='IAF' --dependence_prop=0.5
 python train.py --dependence_type=1 --node_size=10 --seed=4 --flow_type='IAF' --dependence_prop=0.5
 python train.py --dependence_type=1 --node_size=10 --seed=5 --flow_type='IAF' --dependence_prop=0.5
+python train.py --dependence_type=1 --node_size=10 --seed=6 --flow_type='IAF' --dependence_prop=0.5
+
 
 git pull origin run
 git add .
