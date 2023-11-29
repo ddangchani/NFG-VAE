65fbb6597dbac6b1a327e57d4ae9be372e01e8d1
Dangchan <dang11230@gmail.com>
2023-11-29 20:10:23 +0900

    dependece_type=1, node_size=30, seed=1to2, flow_type=IAF,DAGGNN, dependence_prop=0.5, k_max_iter=50, lagrange=0, tau_A=0.1

--- runscript.zsh
+++ runscript.zsh
@@ -1,10 +1,12 @@
 # Script to run experiments
-git pull origin run
+git pull origin train_modification
 
-python train.py --dependence_type=1 --node_size=100 --seed=42 --flow_type='IAF' --dependence_prop=0.5 --k_max_iter=50 --lagrange=0 --tau_A=0.1
-python train.py --dependence_type=1 --node_size=100 --seed=42 --flow_type='DAGGNN' --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --tau_A=0.1
+python train.py --dependence_type=1 --node_size=30 --seed=1 --flow_type='IAF' --dependence_prop=0.5 --k_max_iter=50 --lagrange=0 --tau_A=0.1
+python train.py --dependence_type=1 --node_size=30 --seed=1 --flow_type='DAGGNN' --dependence_prop=0.5 --k_max_iter=50 --lagrange=1
+python train.py --dependence_type=1 --node_size=30 --seed=2 --flow_type='IAF' --dependence_prop=0.5 --k_max_iter=50 --lagrange=0 --tau_A=0.1
+python train.py --dependence_type=1 --node_size=30 --seed=2 --flow_type='DAGGNN' --dependence_prop=0.5 --k_max_iter=50 --lagrange=1
 
-git pull origin run
+git pull origin train_modification
 git add .
-git commit -m "dependece_type=1, node_size=100, seed=42, flow_type=IAF,DAGGNN, dependence_prop=0.5, k_max_iter=50, lagrange=0, tau_A=0.1"
-git push origin run
+git commit -m "dependece_type=1, node_size=30, seed=1to2, flow_type=IAF,DAGGNN, dependence_prop=0.5, k_max_iter=50, lagrange=0, tau_A=0.1"
+git push origin train_modification
