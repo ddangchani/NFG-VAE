39a7a257a5dea3e8f9dddf2ff6341277469efb5a
Dangchan <dang11230@gmail.com>
2023-12-01 15:42:28 +0900

    optimization test run DAGGNN

--- runscript.zsh
+++ runscript.zsh
@@ -1,9 +1,12 @@
 # Script to run experiments
 git pull origin train_modification
 
-python train.py --dependence_type=1 --node_size=30 --seed=4 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.1 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=50 --seed=21 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=50 --seed=31 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=50 --seed=41 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=50 --seed=51 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
 
 git pull origin train_modification
 git add .
-git commit -m "optimization test run"
+git commit -m "optimization test run DAGGNN"
 git push origin train_modification
