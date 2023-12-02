6d72ff61ad005ed47318f72e9b6bb5a2a88ed623
Dangchan <dang11230@gmail.com>
2023-12-02 18:52:45 +0900

    run DAGGNN vs IAF 20 nodes

--- runscript.zsh
+++ runscript.zsh
@@ -1,55 +1,55 @@
 # Script to run experiments
 git pull origin train_modification
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.004 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='IAF' --tau_A=0.016 --loss_prevent=0 --logits=0
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.1 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.3 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.5 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
 
-python train.py --dependence_type=1 --node_size=10 --seed=11 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=21 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=31 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=41 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
-python train.py --dependence_type=1 --node_size=10 --seed=51 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=11 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=21 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=31 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=41 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
+python train.py --dependence_type=1 --node_size=20 --seed=51 --dependence_prop=0.8 --k_max_iter=50 --lagrange=1 --flow_type='DAGGNN'
 
 git pull origin train_modification
 git add .
-git commit -m "run DAGGNN vs IAF 10 nodes"
+git commit -m "run DAGGNN vs IAF 20 nodes"
 git push origin train_modification
